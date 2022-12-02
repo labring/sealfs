b072c72a1d10211c7c6f182e4af38a9ba2d94650
luanshaotong <luanshaotong@163.com>
2022-12-02 07:05:24 +0000

    reverse bit order of int parser

--- common/src/byte.rs
+++ common/src/byte.rs
@@ -3,8 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub fn array2u32(array: &[u8]) -> u32 {
-    (array[0] as u32)
-        + ((array[1] as u32) << 8)
-        + ((array[2] as u32) << 16)
-        + ((array[3] as u32) << 24)
+    (array[3] as u32)
+        + ((array[2] as u32) << 8)
+        + ((array[1] as u32) << 16)
+        + ((array[0] as u32) << 24)
 }
