55e051c9876678f45e8337e3a1eece348b564a76
hui lai <1353307710@qq.com>
2023-01-17 15:48:55 +0800

    block device top abstract interface

---
+++ src/server/storage_engine/block_device/mod.rs
@@ -0,0 +1,8 @@
+// Copyright 2022 labring. All rights reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+*block device is use to bypass filesystem aimed to attain higher performance.
+*/
+pub mod block_storage;
